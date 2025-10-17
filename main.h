872b046f84ffee3e8d2fb3d384ea5ebbfed9d97b
samchristywork <106397573+samchristywork@users.noreply.github.com>
2025-10-17 18:45:15 -0500

    feat: Add data types for editor
    These are the basic data types that the editor will use. Some of these
    are not used yet, and all of these are subject to change.

---
+++ main.h
@@ -0,0 +1,78 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+
+typedef enum { MODE_INSERT, MODE_NORMAL } EditorMode;
+
+typedef struct {
+  size_t height;
+  size_t width;
+  struct termios attrs;
+} Terminal;
+
+typedef struct {
+  char *data;
+  size_t length;
+} Line;
+
+typedef struct {
+  char *name;
+} File;
+
+typedef struct {
+  File file;
+  Line *lines;
+  size_t length;
+} Buffer;
+
+typedef struct {
+  size_t row;
+  size_t column;
+} Cursor;
+
+typedef struct {
+  size_t vertical;
+  size_t horizontal;
+} Scroll;
+
+typedef struct {
+  size_t row;
+  size_t column;
+  size_t height;
+  size_t width;
+  Cursor cursor;
+  Scroll scroll;
+  Buffer *current_buffer;
+} Window;
+
+typedef struct {
+  Terminal terminal;
+  Window **windows;
+  size_t n_windows;
+  size_t current_window;
+  Buffer **buffers;
+  size_t n_buffers;
+  bool running;
+  EditorMode mode;
+} Context;
+
+typedef struct {
+  bool in_keyword;
+  bool in_block_comment;
+  bool in_line_comment;
+  bool in_single_quote_string;
+  bool in_double_quote_string;
+  bool in_preprocessor_directive;
+} SyntaxState;
+
+typedef struct {
+  File *files;
+  size_t length;
+} FileList;
+
+typedef struct {
+  FileList file_list;
+} Arguments;
+
+#endif
